6651dc94fa8d281ae153e73e98210ce3a78670a8
Alkis <a.kafkis@gmail.com>
2025-06-09 18:34:04 +0300

    Add project files.

---
+++ langchain_agent_lab.py
@@ -0,0 +1,28 @@
+# Import relevant functionality
+from langchain_anthropic import ChatAnthropic
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Loads variables from .env
+tavily_api_key = os.getenv("TAVILY_API_KEY")
+
+# Create the agent
+memory = MemorySaver()
+model = ChatAnthropic(model_name="claude-3-sonnet-20240229")
+search = TavilySearchResults(max_results=2, api_key=tavily_api_key)
+tools = [search]
+agent_executor = create_react_agent(model, tools, checkpointer=memory)
+
+
+# Use the agent
+config = {"configurable": {"thread_id": "abc123"}}
+for step in agent_executor.stream(
+    {"messages": [HumanMessage(content="hi im bob! and i live in sf")]},
+    config,
+    stream_mode="values",
+):
+    step["messages"][-1].pretty_print()
